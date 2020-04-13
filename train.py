c5fb569ad15db832e2eebd4d3e87fb8cd67b5fd9
Tao <2761564455@qq.com>
2020-04-13 23:35:27 +0800

    检查prediction和label,检察与name是否对应

--- train.py
+++ train.py
@@ -132,6 +132,7 @@ def train(params, train_loader, validation_loader):
             prediction = F.softmax(output, dim=1)
             prediction = prediction.argmax(dim=1)
             prediction, label = prediction.to('cpu'), label.to('cpu')
+            log("prediction:{},label:{}",(prediction,label),file=log_stream)
             correct += prediction.eq(torch.LongTensor(label)).sum()
 
         else:
@@ -159,6 +160,7 @@ def train(params, train_loader, validation_loader):
                 # Calculating accuracy
                 _, prediction = output.max(dim=1)
                 prediction, label = prediction.to('cpu'), label.to('cpu')
+                log("prediction:{},label:{}",(prediction,label),file=log_stream)
                 correct += prediction.eq(torch.LongTensor(label)).sum()
             else:
                 avg_loss = validating_loss / len(validation_loader)
